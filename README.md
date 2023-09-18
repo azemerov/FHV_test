6bacb5d6ba67bc6f55327cfd1e189a9b41a9d320
Alexander Zemerov <Alexander.Zemerov@aa.com>
2023-09-18 14:28:16 -0500

    initial implementation

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # FHV_test
+Contract Data Engineer assignment.
+The program demostrates a concept of a data loading from For Hire Vehicles dataset and importing of the loaded data into Oracle RDBMS.
+
+Since I have no deeep and recent experience with AWS, a regular Oracle RDBMS was used as a target data storage.
+Instead of specialized SodaPy, standard "requests" library was used. The main reason - the API is quite simple and doesn't require a complext request preparation.
+The program executes an incremental loading, the update timestamp of the most recent record is kept in a local file config.json.
+Since I don't have an account on https://data.cityofnewyork.us/ site, I did'nt use user authentication. A real implemennation should use Authentication header with a registered user token. The token maybe stored in the same config.json file.
+
+Overall program steps -
+Execute an incremental request to the FHV API using last time update timestamp.
+Convert the JSON result into a CSV file (this step can be skipped if there an option to load data directly in CSV format). For the file location we assume an existing directory on Oracle server available via NAS.
+Connect to Oracle and create an external table mapped to the just created CSV file.
+The exteranl table is used as astage table to execute an actual import (using merge statement) into a target table (for this example I assuemed an existance of only two columns in the target table). This step may inlcude some sort of data transformation.
+The external table is dropped.
+The data file can be deleted or preserved just in case of missing/bad data investigation.
+Inported data are available for usage via any SQL-enabled analitical and reporting tools.
+
+
+This implementation only outlines the overall task. A real application should
+1) use more reliable configuration storage. I.e. instead of local config file we may use a DB table
+2) add an user authentication token to the HTTP request
+2) the process should be split between different sub-processes
+	- loading of a new incremental portion and saving it to a local file, move it to "pending" directory
+	- loading of a pending data new data into Oracle table, moving the file into "loaded" or "archive" directory
+	- in case of error, move the file into an "error" directory
+	- optional, an additional data transformation, i.e. join data aquired from different sources
+	- send a notification about success/failure of the execution
+	- if the iteration fails, implement a logic for a retry attemps
+	- ...
---
+++ import_fhv_data.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+
+# make sure to install these packages before running:
+# pip install cx_Oracle
+
+import json
+import csv
+import os.path
+import requests
+import cx_Oracle
+
+with open('settings.json') as json_file:
+    config = json.load(json_file)
+last_ts = config.get("last_ts", "2023-09-18")
+oracle_dir = config["oracle_dir"]
+
+result = requests.get(f"https://data.cityofnewyork.us/resource/8wbx-tsch.json", params={"$where": "last_date_updated >= '2023-09-17T00:00:00.000'", "$order": last_ts})
+if result.statuc_code==200:
+    data = result.json()
+
+    # create new CSV file in the directory on Oracle server ...
+    data_file = open(os.path.join(oracle_dir, 'dhv_data.csv'), 'w')
+    # create the csv writer object
+    csv_writer = csv.writer(data_file)
+
+    count = 0
+    for rec in data:
+
+        if count == 0: # 1st record, prepare CSV header...
+            # Writing headers of CSV file
+            header = rec.keys()
+            csv_writer.writerow(header)
+            count += 1
+     
+        # Writing data of CSV file
+        csv_writer.writerow(rec.values())
+        last_ts = rec["last_ts"]
+
+    data_file.close()
+
+    connection = cx_Oracle.connect(user=config["oracle_user"], password=config["oracle_pswd"], dsn=config["oracle_instance"])
+    cur = connection.cursor()
+    # create a stage table as an external table
+    cur.execute("""CREATE TABLE DHV_DATA (
+      active                 VARCHAR2(5),
+      vehicle_license_number VARCHAR2(15),
+      name                   VARCHAR2(50),
+      license_type           VARCHAR2(15),
+      expiration_date        DATE,
+      permit_license_number  VARCHAR2(20),
+      dmv_license_plate_number VARCHAR2(20),
+      vehicle_vin_number     VARCHAR2(18),
+      certification_date    DATE,
+      hack_up_date          DATE,
+      vehicle_year          VARCHAR2(4),
+      base_number           VARCHAR2(10), 
+      base_name             VARCHAR2(50),
+      base_type             VARCHAR2(20),
+      veh                   VARCHAR2(50),
+      base_telephone_number VARCHAR2(15),
+      base_address          VARCHAR2(100),
+      reason                VARCHAR2(10),
+      last_date_updated     DATE,
+      last_time_updated     DATE
+     )
+  ORGANIZATION EXTERNAL
+    (TYPE ORACLE_LOADER
+     DEFAULT DIRECTORY def_dir_1
+     ACCESS PARAMETERS
+       (RECORDS DELIMITED BY NEWLINE
+        FIELDS (
+          active                 VARCHAR2(5),
+          vehicle_license_number VARCHAR2(15),
+          name                  VARCHAR2(50),
+          license_type          VARCHAR2(15),
+          expiration_date       VARCHAR2(23) date_format DATE mask "yyy-mm-ddThh24:mi:ss.zzz",
+          permit_license_number VARCHAR2(20),
+          dmv_license_plate_number VARCHAR2(20),
+          vehicle_vin_number    VARCHAR2(18),
+          certification_date    VARCHAR2(23) date_format DATE mask "yyy-mm-ddThh24:mi:ss.zzz",
+          hack_up_date          VARCHAR2(23) date_format DATE mask "yyy-mm-ddThh24:mi:ss.zzz",
+          vehicle_year          VARCHAR2(4),
+          base_number           VARCHAR2(10), 
+          base_name             VARCHAR2(50),
+          base_type             VARCHAR2(20),
+          veh                   VARCHAR2(50),
+          base_telephone_number VARCHAR2(15),
+          base_address          VARCHAR2(100),
+          reason                VARCHAR2(10),
+          last_date_updated     VARCHAR2(23) date_format DATE mask "yyy-mm-ddThh24:mi:ss.zzz",
+          last_time_updated     VARCHAR2(23) date_format DATE mask "yyy-mm-ddThh24:mi:ss.zzz"
+        )
+       )
+     LOCATION ('dhv_data.cvs')
+    )""")
+
+    # import into an actual table, we assume only two columnt in the target table - VEHICLE_LICENSE_NUMBER (PK) and NAME
+    cur.execute("""MERGE INTO MY_TARGET_TABLE DST USING (SELECT * FROM DHV_DATA) SRC on (SRC.vehicle_license_number=DST.vehicle_license_number)
+        when matched then
+            update set DST.name = SRC.name, DST.vehicle_year=SRC.vehicle_year
+        when not matched then                     -- if match is not found, insert
+            insert (vehicle_license_number, name, vehicle_year) values (src.vehicle_license_number, src.name, src.vehicle_year)"""
+    
+    # drop the stage table
+    cur.execute("DROP TABLE DHV_DATA")
+
+    # save last TS
+    config.get["last_ts"] = "2023-09-18" //todo:
+    with open('settings.json', 'w') as json_file:
+        json.dump(config, json_file)
+
+else:
+    print("Unable to load FHV data, HTTP status code=", result.statuc_code)
